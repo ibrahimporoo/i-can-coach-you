f92b75f614e3356fe71b0e71c33dfd2c469aed7d
Ibrahim Abdo <ibrahimporoo@gmail.com>
2023-01-17 13:44:57 +0200

    Linking Firebase services with github

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,64 +20,47 @@ const db = getFirestore()
 
 const colRef = collection(db, 'restaurants')
 
-window.onload = () => {
-	let windowLocation = window.location.pathname;
+// selecting the coaches Row
+const coachesContent = document.getElementById('coaches-content');
+let coaches = []; // for fulling firebase' coaches
+// let topCoaches = []; // for fulling firebase' coaches
+let html = '';
+
+async function getData() {
 	// Fetching 'Getting' Data
-	getDocs(colRef)
+	await getDocs(colRef)
 	.then((snapshot) => {
-		let coaches = [];
-		let html = '';
+		// let coaches = [];
 		snapshot.docs.forEach((doc) => {
 			coaches.push({ ...doc.data(), id: doc.id });
 		})
-		if (windowLocation == '/coaches.html') {
-			// selecting the coaches Row
-			const coachesContent = document.getElementById('coaches-content');
-			coaches.map(coach => {
-				html += `
-					<div class="col-lg-4 col-md-6 mb-10">
-						<div class="member" data-aos="zoom-in">
-							<div class="pic"><img src="${coach.photo}" class="img-fluid" alt=""></div>
-								<div class="member-info coaches pricing">
-									<h4>${coach.name}</h4>
-									<span>price: ${coach.price}</span>
-									<p class='detail-item mb-1 mt-1'>Details</p>
-									<span>${coach.category} - ${coach.city} - ${coach.numRating} stars</span>
-									<a href="" target="_blank" class="btn-buy mt-2">Buy Now</a>
-								</div>
+		// topCoaches = coaches.slice(0, 3);
+		coaches.map(coach => {
+			html += `
+				<div class="col-lg-4 col-md-6 mb-10">
+					<div class="member" data-aos="zoom-in">
+						<div class="pic"><img src="${coach.photo}" class="img-fluid" alt=""></div>
+							<div class="member-info coaches pricing">
+								<h4>${coach.name}</h4>
+								<span>price: ${coach.price}</span>
+								<p class='detail-item mb-1 mt-1'>Details</p>
+								<span>${coach.category} - ${coach.city} - ${coach.numRating} stars</span>
+								<a href="" target="_blank" class="btn-buy mt-2">Buy Now</a>
 							</div>
 						</div>
 					</div>
-					`;
-			})
-			coachesContent.innerHTML = '';
-			coachesContent.innerHTML = html;
-		}
-		if (windowLocation == '/index.html') {
-			// selecting the coaches Row
-			const topCoaches = document.getElementById('top-coaches');
-			coaches.slice(0, 3).map(coach => {
-				html += `
-					<div class="col-lg-4 col-md-6 mb-10">
-						<div class="member" data-aos="zoom-in">
-							<div class="pic"><img src="${coach.photo}" class="img-fluid" alt=""></div>
-								<div class="member-info coaches pricing">
-									<h4>${coach.name}</h4>
-									<span>price: ${coach.price}</span>
-									<p class='detail-item mb-1 mt-1'>Details</p>
-									<span>${coach.category} - ${coach.city} - ${coach.numRating} stars</span>
-									<a href="" target="_blank" class="btn-buy mt-2">Buy Now</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					`;
-			})
-			topCoaches.innerHTML = '';
-			topCoaches.innerHTML = html;
-		}
+				</div>
+				`;
+		})
 	})
 	.catch(err => {
-		alert("Error: ", err);
+		console("Error: ", err);
 	})
 }
+
+getData()
+.then(() => {
+	coachesContent.innerHTML = '';
+	coachesContent.innerHTML = html;
+})
+.catch(err => coachesContent.innerHTML = err)
