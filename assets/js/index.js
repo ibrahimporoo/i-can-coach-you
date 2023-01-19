9030cfad0f79c810b3c742f2aa964119695e924d
Ibrahim Abdo <ibrahimporoo@gmail.com>
2023-01-19 17:10:50 +0200

    Some Cutome Items from UI

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,29 +18,36 @@ const db = getFirestore()
 
 const lang = document.querySelector('html').lang;
 
-const colRef = collection(db, 'coaches', 'languages', 'en');
+const colRef = collection(db, 'coaches', 'languages', lang);
 
 // selecting the coaches Row
 const coachesContent = document.getElementById('coaches-content');
-let coaches = []; // for fulling firebase' coaches
-// let topCoaches = []; // for fulling firebase' coaches
+let coaches = []; // for fulling coaches in coaches page
+let topCoachesCount = 3;
 let html = '';
 
 async function getData() {
 	// Fetching 'Getting' Data
 	await getDocs(colRef)
 	.then((snapshot) => {
+		// Check if we in the home page or top coaches page
 		snapshot.docs.forEach((doc) => {
 			coaches.push({ ...doc.data(), id: doc.id });
 		})
-		console.log(coaches);
+		// Adding Content of Data coming from Firebase to HTML
+		if(coachesContent.classList.contains('top-coaches')){
+			coaches = coaches.slice(0, topCoachesCount)
+		}
 		coaches.map(coach => {
 			html += `
 				<div class="col-lg-4 col-md-6 mb-10">
 					<div class="member" data-aos="zoom-in">
 						<div class="pic"><img src="${coach.image}" class="img-fluid" alt="Coach Image"></div>
 							<div class="member-info coaches pricing">
-								<h4>${coach.name} - ${coach.jobTitle}</h4>
+								<div class='ps-3 pe-3'>
+									<h5>${coach.name}</h5>
+									<h4>${coach.jobTitle}</h4>
+								</div>
 								<span>price: ${coach.pricing}</span>
 								<p class='detail-item mb-1 mt-1'>Details</p>
 								<span>${coach.category}</span>
@@ -58,56 +65,11 @@ async function getData() {
 		})
 	})
 	.catch(err => {
-		console.log("Error: " + err);
+		alert("Error: " + err);
 	})
-	/*
-		{
-    "country": "مصر",
-    "summary": "",
-    "category": "المبيعات والتسويق، إدارة الأعمال، التكنولوجيا، العمليات، الشؤون القانونية، ريادة الأعمال ، تنمية الأطفال، أسلوب الحياة والصحة",
-    "rating": "5",
-    "jobTitle": "مساعد رئيس تنفيذي، مؤسس/صاحب عمل",
-    "city": "القاهرة",
-    "image": "https://drive.google.com/file/d/10QyOuOvjkgjD-qmWuT8isDZLMbngdBiq/view?usp=share_link",
-    "paymentLink": "https://easykash.net/TEI2615",
-    "pricing": "500 حنية مصري",
-    "name": "أدهم",
-    "order": "3",
-    "SM_accounts": "https://www.linkedin.com/in/dr-adham-molokhia-752282b1/",
-    "id": "I2dR6tDU4nlG1GozazEk"
-}
-	*/ 
 }
 
 getData().then(() => {
 	coachesContent.innerHTML = ''
 	coachesContent.innerHTML = html
 });
-
-
-// getDocs(colRef).then((snapshot) => {
-// 	console.log(snapshot);
-// 	console.log(snapshot.docs);
-// })
-
-// const colRef = collection(db, `/restaurants/1G3OUuNQw80cKcIZu9E6/exams`);
-// const colRef = collection(db, `restaurants`);
-
-// getDocs(colRef).then((snapshot) => {
-// 	console.log(snapshot);
-// 	console.log(snapshot.docs);
-// 	console.log(snapshot.docs[0]);
-// 	console.log(snapshot.docs[0].id);
-// })
-
-// const docRef = doc(db, "restaurants", "languages");
-// const docSnap = await getDoc(docRef);
-
-// if (docSnap.exists()) {
-//   console.log("Document data:", docSnap.data());
-// } else {
-//   // doc.data() will be undefined in this case
-//   console.log("No such document!");
-// }
-
-
